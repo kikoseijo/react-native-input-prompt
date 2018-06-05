3f414cd195f973a2922602244ea250d2f79e0609
Kiko Seijo <kikoseijo@users.noreply.github.com>
2018-06-05 22:42:24 +0200

    Flow defs + dialogStyle
    Moved to React flow@
    Added a prop to customise dialog style when working on large devices.

--- src/Prompt.js
+++ src/Prompt.js
@@ -1,44 +1,42 @@
-import React, { Component } from "react";
-import {
-	View,
-	Text,
-	Button,
-	TextInput,
-	Dimensions,
-	Modal,
-	TouchableOpacity
-} from "react-native";
-import styles from "./styles";
-import PropTypes from "prop-types";
+// @flow
+import * as React from 'react';
+import { View, Text, Button, TextInput, Dimensions, Modal, TouchableOpacity } from 'react-native';
+import styles from './Prompt.style';
 
-export default class Prompt extends Component {
-	static propTypes = {
-		title: PropTypes.string.isRequired,
-		visible: PropTypes.bool.isRequired,
-		placeholder: PropTypes.string.isRequired,
-		cancelText: PropTypes.string,
-		submitText: PropTypes.string,
-		cancelButtonStyle: PropTypes.object,
-		submitButtonStyle: PropTypes.object,
-		cancelButtonTextStyle: PropTypes.object,
-		submitButtonTextStyle: PropTypes.object,
-		titleStyle: PropTypes.object,
-		onChangeText: PropTypes.func,
-		onSubmit: PropTypes.func.isRequired,
-		onCancel: PropTypes.func.isRequired
-	};
+type Props = {
+	title: string,
+	visible: boolean,
+	placeholder: string,
+	cancelText?: string,
+	submitText?: string,
+	cancelButtonStyle?: Object | number,
+	submitButtonStyle?: Object | number,
+	cancelButtonTextStyle?: Object | number,
+	submitButtonTextStyle?: Object | number,
+	titleStyle?: Object | number,
+	dialogStyle?: Object | number,
+	onChangeText?: Function,
+	onSubmit: Function,
+	onCancel: Function
+};
 
+type State = {
+	value: string
+};
+
+export default class Prompt extends React.Component<Props, State> {
 	static defaultProps = {
-		title: "Title",
+		title: 'Title',
 		visible: false,
-		placeholder: "Placeholder",
-		cancelText: "Cancel",
-		submitText: "Submit",
+		placeholder: 'Placeholder',
+		cancelText: 'Cancel',
+		submitText: 'Submit',
 		cancelButtonStyle: {},
 		cancelButtonTextStyle: {},
 		submitButtonStyle: {},
 		submitButtonTextStyle: {},
 		titleStyle: {},
+		dialogStyle: {},
 		onChangeText: () => {},
 		onSubmit: () => {},
 		onCancel: () => {}
@@ -46,24 +44,24 @@ export default class Prompt extends Component {
 	constructor() {
 		super();
 		this.state = {
-			value: ""
+			value: ''
 		};
 	}
 	_onChangeText(value) {
 		this.setState({
 			value: value
 		});
-		this.props.onChangeText(value);
+		this.props.onChangeText && this.props.onChangeText(value);
 	}
 
 	_onSubmit() {
 		this.props.onSubmit(this.state.value);
-		this.setState({ value: "" });
+		this.setState({ value: '' });
 	}
 
 	_onCancel() {
 		this.props.onCancel();
-		this.setState({ value: "" });
+		this.setState({ value: '' });
 	}
 	render() {
 		return (
@@ -73,10 +71,8 @@ export default class Prompt extends Component {
 				visible={this.props.visible}
 				onRequestClose={this._onCancel.bind(this)}>
 				<View style={styles.screenOverlay}>
-					<View style={styles.dialogPrompt}>
-						<Text style={[styles.title, this.props.titleStyle]}>
-							{this.props.title}
-						</Text>
+					<View style={[styles.dialogPrompt, this.props.dialogStyle]}>
+						<Text style={[styles.title, this.props.titleStyle]}>{this.props.title}</Text>
 						<TextInput
 							placeholder={this.props.placeholder}
 							style={styles.textInput}
@@ -87,31 +83,17 @@ export default class Prompt extends Component {
 						<View style={styles.buttonsOuterView}>
 							<View style={styles.buttonsInnerView}>
 								<TouchableOpacity
-									style={[
-										styles.button,
-										this.props.cancelButtonStyle
-									]}
+									style={[styles.button, this.props.cancelButtonStyle]}
 									onPress={this._onCancel.bind(this)}>
-									<Text
-										style={[
-											styles.cancelButtonText,
-											this.props.cancelButtonTextStyle
-										]}>
+									<Text style={[styles.cancelButtonText, this.props.cancelButtonTextStyle]}>
 										{this.props.cancelText}
 									</Text>
 								</TouchableOpacity>
 								<View style={styles.buttonsDivider} />
 								<TouchableOpacity
-									style={[
-										styles.button,
-										this.props.submitButtonStyle
-									]}
+									style={[styles.button, this.props.submitButtonStyle]}
 									onPress={this._onSubmit.bind(this)}>
-									<Text
-										style={[
-											styles.submitButtonText,
-											this.props.submitButtonTextStyle
-										]}>
+									<Text style={[styles.submitButtonText, this.props.submitButtonTextStyle]}>
 										{this.props.submitText}
 									</Text>
 								</TouchableOpacity>
